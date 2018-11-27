e7842de7af20bc4367bfaedb2154f2f5c6448028
Tara Sabella <tarasabella@gmail.com>
2018-11-27 13:31:44 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -21,7 +21,7 @@ end
 
 def alan_kay_is_known_for
 	# What combination of keys would you use to return the value of the :known_for key of :alan_kay?
-	alan_kay_is_known_for = programmer_hash[:known_for]
+	alan_kay_is_known_for = programmer_hash[:alan_kay]
 	
 	programmer_hash = 
  		{
